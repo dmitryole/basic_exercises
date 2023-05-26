9d3b177d552cf436da3ecc8228301605ad3d4c6b
DmitrOle <dimoleser@gmail.com>
2023-05-26 09:35:20 +0300

    Готовая домашняя работа

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -4,7 +4,8 @@
 # Вася: 1
 # Маша: 2
 # Петя: 2
-
+from pprint import pprint
+from collections import Counter
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -12,7 +13,13 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+
+names = []
+for student in students:
+    names += [student['first_name']] #['Вася', 'Петя', 'Маша', 'Маша', 'Петя']
+
+for key, value in (Counter(names).items()): #dict_items([('Вася', 1), ('Петя', 2), ('Маша', 2)])
+    print(f'{key}: {value}')
 
 
 # Задание 2
@@ -26,14 +33,25 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+names = []
+for student in students:
+    names += [student['first_name']] #['Вася', 'Петя', 'Маша', 'Маша', 'Оля']
 
+n = Counter(names).most_common()[0] #('Маша', 2)
+print(f'Самое частое имя среди учеников: {n[0]}')
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
+def classs_search(school, i = 0, names = []):
+    for classs in school:
+        i += 1
+        for student in classs:
+            names += [student['first_name']]
+        n = Counter(names).most_common()[0]
+        print(f'Самое частое имя в классе {i}: {n[0]}')
 
 school_students = [
     [  # это – первый класс
@@ -51,7 +69,7 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+classs_search(school_students)
 
 
 # Задание 4
@@ -72,7 +90,18 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+for classs in school:
+    names = []
+    for student in classs['students']:
+        names += [student['first_name']]
+        boy = 0
+        girl = 0
+        for gendr in names:
+            if is_male[gendr] == True:
+                boy += 1
+            elif is_male[gendr] == False:
+                girl += 1
+    print(f'{classs["class"]}: девочки {girl}, мальчики {boy}')
 
 
 # Задание 5
@@ -91,5 +120,25 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
+for classs in school:
+    gendrs = {'class': '', 'man': 0, 'woman': 0}
+    gendrs['class'] = classs['class']
+    for student in classs['students']:
+        name = student['first_name']
+        if is_male[name] == False:
+            gendrs['woman'] += 1
+        elif is_male[name] == True:
+            gendrs['man'] += 1
+    gendrs_class.append(gendrs)
 
+print(gendrs_class) # [{'class': '2a', 'man': 0, 'woman': 2}, {'class': '3c', 'man': 2, 'woman': 0}]
+
+i = 0
+if gendrs_class[i]['man'] > gendrs_class[i+1]['man']:
+    print(f'Больше всего мальчиков в классе {gendrs_class[i]["class"]}')
+elif gendrs_class[i]['man'] < gendrs_class[i+1]['man']:
+    print(f'Больше всего мальчиков в классе {gendrs_class[i+1]["class"]}')
+if gendrs_class[i]['woman'] > gendrs_class[i+1]['woman']:
+    print(f'Больше всего девочек в классе {gendrs_class[i]["class"]}')
+elif gendrs_class[i]['woman'] < gendrs_class[i+1]['woman']:
+    print(f'Больше всего девочек в классе {gendrs_class[i+1]["class"]}')
